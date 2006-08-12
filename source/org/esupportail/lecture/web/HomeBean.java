2c3c184154ebf4a2a94fcb6fc79b0d52057e2438
Raymond Bourges <raymond.bourges@univ-rennes1.fr>
2006-08-12 11:21:05 +0000

    Add ant instruction in order to have goob xml configuration files (with developer.pseudo in build.properties files)

--- source/org/esupportail/lecture/web/HomeBean.java
+++ source/org/esupportail/lecture/web/HomeBean.java
@@ -1,14 +1,22 @@
 /**
-* ESUP-Portail Lecture - Copyright (c) 2006 ESUP-Portail consortium
-* For any information please refer to http://esup-helpdesk.sourceforge.net
-* You may obtain a copy of the licence at http://www.esup-portail.org/license/
-*/
+ * ESUP-Portail Lecture - Copyright (c) 2006 ESUP-Portail consortium
+ * For any information please refer to http://esup-helpdesk.sourceforge.net
+ * You may obtain a copy of the licence at http://www.esup-portail.org/license/
+ */
 package org.esupportail.lecture.web;
 
 import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.esupportail.lecture.domain.model.Category;
+import org.esupportail.lecture.domain.model.ChannelConfig;
 
 public class HomeBean {
+	/**
+	 * Log instance 
+	 */
+	protected static final Log log = LogFactory.getLog(ChannelConfig.class);
 	private List<Category> categories;
 	private FacadeWeb facadeWeb;
 	
@@ -17,10 +25,13 @@ public class HomeBean {
 	}
 	
 	public List<Category> getCategories() {
+		if (log.isDebugEnabled()) {
+			log.debug("In getCategories");
+		}
 		this.categories = facadeWeb.getFacadeService().getCategories();
 		return this.categories;
 	}
-
+	
 	public void setCategories(List<Category> categories) {
 		this.categories = categories;
 	}
