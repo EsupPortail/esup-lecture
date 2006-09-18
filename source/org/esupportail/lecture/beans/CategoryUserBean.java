b29ee294302c73cbd5054934d4bf036f8b3e4ee4
Gwenaelle Bouteille <gwenaelle.bouteille@univ-rennes1.fr>
2006-09-18 08:12:57 +0000

    Domain code to display categories on interface, but no displaying yet

---
+++ source/org/esupportail/lecture/beans/CategoryUserBean.java
@@ -0,0 +1,25 @@
+package org.esupportail.lecture.beans;
+
+public class CategoryUserBean {
+	/**
+	 * Name of the category
+	 */
+	private String name;
+
+	/**
+	 * @return Returns the name.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name The name to set.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
