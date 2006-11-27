cc434f4f6f8353c1c975bb6e8d51448c6751215b
Gwenaelle Bouteille <gwenaelle.bouteille@univ-rennes1.fr>
2006-11-27 14:15:36 +0000

    Refactoring on services getVisibleSources, getItems,...

---
+++ src/org/esupportail/lecture/exceptions/CustomSourceNotFoundException.java
@@ -0,0 +1,9 @@
+package org.esupportail.lecture.exceptions;
+
+public class CustomSourceNotFoundException extends Exception {
+
+	public CustomSourceNotFoundException(String string) {
+		super(string);
+	}
+
+}
---
+++ src/org/esupportail/lecture/exceptions/SourceNotLoadedException.java
@@ -0,0 +1,9 @@
+package org.esupportail.lecture.exceptions;
+
+public class SourceNotLoadedException extends Exception {
+
+	public SourceNotLoadedException(String string) {
+		super(string);
+	}
+
+}
