6ed4b1d9fc96d1fe1adb5b1239836d80b6c62559
Gwenaelle Bouteille <gwenaelle.bouteille@univ-rennes1.fr>
2007-10-21 09:58:36 +0000

    Préparation pour faire les drivers de test du métier

---
+++ test/org/esupportail/lecture/domain/DomainServiceImplTest.java
@@ -0,0 +1,175 @@
+/**
+* ESUP-Portail Lecture - Copyright (c) 2006 ESUP-Portail consortium
+* For any information please refer to http://esup-lecture.sourceforge.net
+* You may obtain a copy of the licence at http://www.esup-portail.org/license/
+*/
+/**
+*
+*/
+
+package org.esupportail.lecture.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *  Test de la classe service du métier
+ * @author gwen
+ *
+ */
+public class DomainServiceImplTest {
+
+	
+	/* 
+	 *************************** PROPERTIES ******************************** */
+	
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#getConnectedUser(java.lang.String)}.
+	 */
+	@Test
+	public void testGetConnectedUser() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#getContext(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testGetContext() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#getAvailableCategories(java.lang.String, java.lang.String, org.esupportail.lecture.domain.ExternalService)}.
+	 */
+	@Test
+	public void testGetAvailableCategories() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#getAvailableSources(java.lang.String, java.lang.String, org.esupportail.lecture.domain.ExternalService)}.
+	 */
+	@Test
+	public void testGetAvailableSources() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#getItems(java.lang.String, java.lang.String, org.esupportail.lecture.domain.ExternalService)}.
+	 */
+	@Test
+	public void testGetItems() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#marckItemReadMode(java.lang.String, java.lang.String, java.lang.String, boolean)}.
+	 */
+	@Test
+	public void testMarckItemReadMode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#markItemDisplayMode(java.lang.String, java.lang.String, org.esupportail.lecture.domain.model.ItemDisplayMode)}.
+	 */
+	@Test
+	public void testMarkItemDisplayMode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#setTreeSize(java.lang.String, java.lang.String, int)}.
+	 */
+	@Test
+	public void testSetTreeSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#foldCategory(java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testFoldCategory() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#unfoldCategory(java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testUnfoldCategory() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#getVisibleCategories(java.lang.String, java.lang.String, org.esupportail.lecture.domain.ExternalService)}.
+	 */
+	@Test
+	public void testGetVisibleCategories() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#getVisibleSources(java.lang.String, java.lang.String, org.esupportail.lecture.domain.ExternalService)}.
+	 */
+	@Test
+	public void testGetVisibleSources() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#subscribeToCategory(java.lang.String, java.lang.String, java.lang.String, org.esupportail.lecture.domain.ExternalService)}.
+	 */
+	@Test
+	public void testSubscribeToCategory() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#subscribeToSource(java.lang.String, java.lang.String, java.lang.String, org.esupportail.lecture.domain.ExternalService)}.
+	 */
+	@Test
+	public void testSubscribeToSource() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#unsubscribeToCategory(java.lang.String, java.lang.String, java.lang.String, org.esupportail.lecture.domain.ExternalService)}.
+	 */
+	@Test
+	public void testUnsubscribeToCategory() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.esupportail.lecture.domain.DomainServiceImpl#unsubscribeToSource(java.lang.String, java.lang.String, java.lang.String, org.esupportail.lecture.domain.ExternalService)}.
+	 */
+	@Test
+	public void testUnsubscribeToSource() {
+		fail("Not yet implemented");
+	}
+
+	
+}
