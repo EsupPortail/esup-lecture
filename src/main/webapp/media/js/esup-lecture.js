36dce4272f8914f9ed03bf3ce18a02c7401592dd
Raymond Bourges <raymond.bourges@univ-rennes1.fr>
2011-03-16 15:33:01 +0000

    all js code in an unique esup-lecture.js file

---
+++ src/main/webapp/media/js/esup-lecture.js
@@ -0,0 +1,11 @@
+(function($){
+$(document).ready(function() {  
+	$(".itemDisplayModeSelector").change(function () {
+		$(".itemDisplayModeButton").click();
+	});
+
+	$(".itemDisplayModeButton").hide();
+
+});
+
+})(jQuery);
