fced79c73642f854a80222b63fcf25549fc13a2a
nhenry <nadia.henry@univ-rennes1.fr>
2015-04-28 15:05:03 +0200

    update portlet-size

--- src/main/webapp/app/js/portlet-size.js
+++ src/main/webapp/app/js/portlet-size.js
@@ -1,24 +1,33 @@
-var $portletContainers;
+(function($){
 
-$(document).ready(function() {
-    $portletContainers = $(".portlet-container");
-// Resize event isn't fired on DOM Content Loaded, we launch the function manually
-    onWindowResize();
-    $(window).resize(onWindowResize);
-});
+    var $portletContainers;
 
-function onWindowResize() {
-    $portletContainers.each(function(index) {
-        var $that = $(this);
-        var portletWidth = $that.width()
-        $that.removeClass("xs sm md lg");
-        if(portletWidth < 768)
-            $that.addClass("xs");
-        if(portletWidth >= 768 && portletWidth < 992)
-            $that.addClass("sm");
-        if(portletWidth >= 992 && portletWidth < 1200)
-            $that.addClass("md");
-        if(portletWidth >= 1200)
-            $that.addClass("lg");
+    $(document).ready(function() {
+        $portletContainers = $('.portlet-container');
+        $(window).resize(onWindowResize);
+        onWindowResize();
     });
-}
+
+    function onWindowResize() {
+
+        $portletContainers.each(function(index) {
+            var $that = $(this);
+            var portletWidth = $that.width();
+
+            $that.removeClass("xs sm md lg");
+            if(portletWidth < 768) {
+                $that.addClass("xs");
+            }
+            if(portletWidth >= 768 && portletWidth < 992) {
+                $that.addClass("sm");
+            }
+            if(portletWidth >= 992 && portletWidth < 1200) {
+                $that.addClass("md");
+            }
+            if(portletWidth >= 1200) {
+                $that.addClass("lg");
+            }
+        });
+    }
+
+})(jQuery);
